063afc3fb793417fab58ecbd001e719b86e5e381
Nik Bagramyan <nikbagramyan@Nik-Azbekyans-MacBook-Pro.local>
2019-12-19 12:02:12 -0800

    detail link

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render
+from .models import Shoe
 # Add the following import
-class Shoe:
-    def __init__(self, brand, name, style, description):
-        self.brand = brand
-        self.name = name
-        self.style = style
-        self.description = description
-
-shoes = [
-    Shoe('Adidas', 'NMD', 'running', 'sick!')
-    ]
 
 # Define the home view
 def home(request):
     return render(request, 'home.html')
 
+# Create your views here.
 def about(request):
     return render(request, 'about.html')
 
 def shoes_index(request):
+    shoes = Shoe.objects.all()
     return render(request, 'shoes/index.html', {
         'shoes': shoes
     })
-# Create your views here.
+
+def shoes_detail(request, shoe_id):
+    shoe = Shoe.objects.get(id=shoe_id)
+    return render(request, 'shoes/detail.html', {
+        'shoe': shoe
+    })
